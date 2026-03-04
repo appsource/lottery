e1970bdeb058fe647e0b2be47ac48a8c33a39735
LotteryFactory CI <ci@noxon.digital>
2026-03-04 22:30:08 +0300

    deploy: replace Next.js admin panel with React lottery frontend (BSC testnet)
    - chainId: 97 (BSC Testnet)
    - contract: 0xa4729b743579d46de8edfece6e1fa2ff2f2fb631
    - token: WEENUS 0x703f112Bda4Cc6cb9c5FB4B2e6140f6D8374F10b
    - lottery round 2 is open
    
    Built from noxonsu/LotteryFactory frontend/build/

---
+++ static/js/5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[5],{699:function(n,p){},709:function(n,p){}}]);
+//# sourceMappingURL=5.chunk.js.map
--- vendor/7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[7],{726:function(n,p){}}]);
-//# sourceMappingURL=7.chunk.js.map
